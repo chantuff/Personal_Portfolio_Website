e74d563c088583a0dc244db1f63aa4877529769b
chantuff <chan.tuff@yahoo.com>
2023-12-22 13:31:21 -0500

    Added nav and some css for home page

--- Portfolio_Website/src/App.jsx
+++ Portfolio_Website/src/App.jsx
@@ -8,26 +8,18 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='container'>
+      <div class="vertical-center">
+      <h1 className='header'>Chan's Portfolio</h1>
+
+      <div className='nav'> 
+        <a className='active' href='#home'>Home</a>
+        <a href='#portfolio'>Portfolio</a>
+        <a href='#resume'>Resume</a>
+        <a href='#contact'>Contact</a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      </div> 
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
