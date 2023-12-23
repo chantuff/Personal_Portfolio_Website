4dab76fd96482e11561dfcb19c9a624579ce72eb
chantuff <chan.tuff@yahoo.com>
2023-12-23 15:00:15 -0500

    Added deployed project

--- Portfolio_Website/src/pages/Projects.jsx
+++ Portfolio_Website/src/pages/Projects.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 function Projects() {
   return (
-    <div>Projects</div>
+    <div className='projects'>Projects <br></br>
+    <br></br>
+<div className='project-box'>
+      <a href='https://chans-weather-project.netlify.app' target='blank' >Weather App</a>
+      <p className='about-weather-app'>
+        This React project was created using Vite. I also used the Openweather Map API to pull in the weather data. 
+      </p>
+      </div>
+    </div>
+   
   )
 }
 
